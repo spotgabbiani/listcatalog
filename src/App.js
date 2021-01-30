111c46ae75b099a9f6398a3fbc0bec611ddfaf9f
Arnoldo Montano <arnoldo@Arnoldos-MacBook-Pro.local>
2021-01-29 19:11:54 -0700

    Added the functionality for arrow buttons

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import React, { Component, Fragment } from 'react';
-import previousIcon from './assets/icons/left-icon.png';
-import nextIcon from './assets/icons/right_icon.png';
-import thumb1 from './assets/images/thumb/tea-light-thumb.jpeg';
-import thumb2 from './assets/images/thumb/white-light-thumb.jpeg';
-import thumb3 from './assets/images/thumb/pink-light-thumb.jpeg';
-import thumb4 from './assets/images/thumb/tea-light-thumb.jpeg';
-import image1 from './assets/images/tea-light.jpeg';
-import image2 from './assets/images/white-light.jpeg';
-import image3 from './assets/images/pink-light.jpeg';
-import image4 from './assets/images/tea-light.jpeg';
+import React, { Component, Fragment } from "react";
+import previousIcon from "./assets/icons/left-icon.png";
+import nextIcon from "./assets/icons/right_icon.png";
+import thumb1 from "./assets/images/thumb/tea-light-thumb.jpeg";
+import thumb2 from "./assets/images/thumb/white-light-thumb.jpeg";
+import thumb3 from "./assets/images/thumb/pink-light-thumb.jpeg";
+import thumb4 from "./assets/images/thumb/tea-light-thumb.jpeg";
+import image1 from "./assets/images/tea-light.jpeg";
+import image2 from "./assets/images/white-light.jpeg";
+import image3 from "./assets/images/pink-light.jpeg";
+import image4 from "./assets/images/tea-light.jpeg";
 
-import './App.css';
+import "./App.css";
 import Viewer from "./components/Viewer";
 import Thumbs from "./components/Thumbs";
-
 
 const catalogs = [
   {
     thumb: thumb1,
-    image: image1
+    image: image1,
   },
   {
     thumb: thumb2,
-    image: image2
+    image: image2,
   },
   {
     thumb: thumb3,
-    image: image3
+    image: image3,
   },
   {
     thumb: thumb4,
-    image: image4
-  }
+    image: image4,
+  },
 ];
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Catalog Viewer',
+      title: "Catalog Viewer",
       catalogs: [...catalogs],
       currentIndex: -1,
       catalogSelected: catalogs[3],
@@ -56,56 +54,88 @@ class App extends Component {
   }
 
   selectedCatalog(index) {
-
+    this.setState({ catalogSelected: catalogs[this.currentIndex] });
   }
 
   previousClick() {
+    const newIndex =
+      this.state.currentIndex === 0 ? 3 : this.state.currentIndex - 1;
 
+    this.setState({
+      currentIndex: newIndex,
+      catalogSelected: catalogs[newIndex],
+    });
   }
 
   nextClick() {
+    const newIndex =
+      this.state.currentIndex === 3 ? 0 : this.state.currentIndex + 1;
 
+    this.setState({
+      currentIndex: newIndex,
+      catalogSelected: catalogs[newIndex],
+    });
   }
 
   slideChange(event) {
-
+    event.preventDefault();
+    this.setState({ slideActive: !this.state.slideActive });
   }
 
-  resetSlideTimer(isActive = false) {
+  resetSlideTimer(isActive = false) {}
 
-  }
-
-  onSlideChange() {
-
-  }
+  onSlideChange() {}
 
   render() {
+    console.log(this.state.slideActive);
     return (
-        <Fragment>
-          <div className="title" data-testid="app-title"> {this.state.title} </div>
-          <div className="catalog-outer">
-            <div className="catalog-view">
-              <div className="text-center">
-                <div className="view-outter text-center">
-                  <Viewer catalog={this.state.catalogSelected.image}/>
-                </div>
+      <Fragment>
+        <div className="title" data-testid="app-title">
+          {" "}
+          {this.state.title}{" "}
+        </div>
+        <div className="catalog-outer">
+          <div className="catalog-view">
+            <div className="text-center">
+              <div className="view-outter text-center">
+                <Viewer catalog={this.state.catalogSelected.image} />
               </div>
-            </div>
-            <div className="catalog-items">
-              <div className="previous" onClick={this.previousClick} data-testid="prev-icon">
-                <img src={previousIcon}/>
-              </div>
-              <div className="next" onClick={this.nextClick} data-testid="next-icon">
-                <img src={nextIcon}/>
-              </div>
-              <Thumbs items={this.state.catalogs} currentIndex={this.state.currentIndex} selectedCatalog={this.selectedCatalog}/>
-            </div>
-
-            <div className="slide-input">
-              <input type="checkbox" onChange={this.slideChange} className="test" data-testid="slide"/> Slide
             </div>
           </div>
-        </Fragment>
+          <div className="catalog-items">
+            <div
+              className="previous"
+              onClick={this.previousClick}
+              data-testid="prev-icon"
+            >
+              <img src={previousIcon} />
+            </div>
+            <div
+              className="next"
+              onClick={this.nextClick}
+              data-testid="next-icon"
+            >
+              <img src={nextIcon} />
+            </div>
+            <Thumbs
+              items={this.state.catalogs}
+              currentIndex={this.state.currentIndex}
+              selectedCatalog={this.selectedCatalog}
+            />
+          </div>
+
+          <div className="slide-input">
+            <input
+              type="checkbox"
+              onChange={this.slideChange}
+              className="test"
+              data-testid="slide"
+              checked={this.state.slideActive}
+            />{" "}
+            Slide
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
--- src/components/Thumbs.js
+++ src/components/Thumbs.js
@@ -1,29 +1,43 @@
-import React, { Component, Fragment } from 'react';
-import './Thumbs.css';
+import React, { Component, Fragment } from "react";
+import "./Thumbs.css";
 
 class Thumbs extends Component {
-    constructor(props){
-        super(props);
-        this.onImageSelect = this.onImageSelect.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.onImageSelect = this.onImageSelect.bind(this);
+  }
 
-    onImageSelect(event) {
-
-    }
+  onImageSelect(event) {}
 
   render() {
     return (
-        <Fragment>
-            {
-                this.props.items.map((catalog,idx) => (
-                    <span className="catalog item"  onClick={this.onImageSelect} className={'thumb-select'} id={idx} key={idx} data-testid={'thumb_outer_'+idx}>
-                        <span className={"thumb-outer " + (idx == this.props.currentIndex ? 'thumb-selected' : ' ')} data-testid={'thumb_'+idx}>
-                            <span className="thumb" id={idx} style={{ backgroundImage: 'url('+ catalog.thumb + ')'}} data-testid={'thumb_img_'+idx} />
-                        </span>
-                    </span>
-                ))}
-
-        </Fragment>
+      <Fragment>
+        {this.props.items.map((catalog, idx) => (
+          <span
+            className="catalog item"
+            onClick={this.onImageSelect}
+            className={"thumb-select"}
+            id={idx}
+            key={idx}
+            data-testid={"thumb_outer_" + idx}
+          >
+            <span
+              className={
+                "thumb-outer " +
+                (idx == this.props.currentIndex ? "thumb-selected" : " ")
+              }
+              data-testid={"thumb_" + idx}
+            >
+              <span
+                className="thumb"
+                id={idx}
+                style={{ backgroundImage: "url(" + catalog.thumb + ")" }}
+                data-testid={"thumb_img_" + idx}
+              />
+            </span>
+          </span>
+        ))}
+      </Fragment>
     );
   }
 }
